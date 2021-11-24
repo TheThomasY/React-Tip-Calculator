400548fc21104d794b91ed836403bf865a330410
TheThomasY <twy1g16@soton.ac.uk>
2021-11-24 16:33:09 +0000

    style: mobile layout done

--- src/components/NoOfPeople.js
+++ src/components/NoOfPeople.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import person from '../images/icon-person.svg';
 import './scss/NoOfPeople.css';
 
 export default function NoOfPeople() {
   return (
     <div className='no-of-people'>
       <h4 className='section-title'>Number of People</h4>
+      <img className='input-icon icon-person' src={person} alt='Person icon' />
+      <input className='input-bar' type='text' />
     </div>
   );
 }
